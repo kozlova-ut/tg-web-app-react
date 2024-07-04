68cd103684411e1b538c6e4c44516c799eb0beda
Yulia Kozlova <twnthr@gmail.com>
2024-07-04 15:05:27 +0300

    'new'

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
     <div className="App">
       <Header />
       <Routes>
-        <Route index element={<ProductList />}/>
+        <Route path={'/'} element={<ProductList />}/>
         <Route path={'form'} element={<Form />}/>
       </Routes>
     </div>
