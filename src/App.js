9a34f2c2a66d164d035b5ec050dcbc113805abeb
Yulia Kozlova <twnthr@gmail.com>
2024-07-08 16:56:46 +0300

    'new'

--- src/App.js
+++ src/App.js
@@ -1,27 +1,20 @@
 import { useEffect } from 'react';
 import './App.css';
-import { useTelegram } from './hooks/useTelegram';
-import Header from './components/Header/Header';
-import { Route, Routes } from 'react-router-dom';
-import ProductList from './components/ProductList/ProductList';
-import Form from './components/Form/Form';
+
 
 function App() {
-  const {tg} = useTelegram();
 
-  useEffect(() => {
-    tg.ready();
-  }, [])
+    useEffect(() => {
+        tg.ready();
+    }, [])
 
-  return (
-    <div className="App">
-      <Header />
-      <Routes>
-        <Route index element={<ProductList />}/>
-        <Route path={'form'} element={<Form />}/>
-      </Routes>
-    </div>
-  );
+
+
+    return (
+        <div className='App'>
+            
+        </div>
+    )
 }
 
 export default App;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,20 @@
 import React from "react";
 import Button from "../Button/Button";
-import { useTelegram } from "../../hooks/useTelegram";
-import './Header.css';
 
 const Header = () => {
-    const {user, onClose} = useTelegram();
+    const tg = window.Telegram.WebApp;
+    const onClose = () => {
+        tg.close();
+    }
 
     return (
         <div className={'header'}>
             <Button onClick={onClose}>Закрыть</Button>
             <span className={'username'}>
-                {user?.username}
+                {tg.initDataUnsafe?.user?.username}
             </span>
         </div>
-    );
-};
+    )
+}
 
 export default Header;
