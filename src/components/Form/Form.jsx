ad707f30a079a2db5ba57c44ac217268c6494110
Yulia Kozlova <twnthr@gmail.com>
2024-07-10 14:57:59 +0300

    'new'

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -19,9 +19,9 @@ const Form = () => {
     }, [tg, name, surname, gender])
 
     useEffect(() => {
-        tg.WebApp.onEvent('mainButtonCkecked', onSendData);
+        tg.WebApp.onEvent('mainButtonClicked', onSendData);
         return () => {
-            tg.WebApp.offEvent('mainButtonCkecked', onSendData);
+            tg.WebApp.offEvent('mainButtonClicked', onSendData);
         }
     }, [tg, onSendData])
 
